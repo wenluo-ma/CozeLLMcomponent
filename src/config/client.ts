242902e76395f1ab589e6d4b955f92a1f8752e52
wenluo-ma <139756383+wenluo-ma@users.noreply.github.com>
2024-12-01 17:00:15 +0800

    client.ts

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,7 +1,7 @@
 import { CozeAPI, COZE_CN_BASE_URL } from '@coze/api'
 export const client = new CozeAPI({
-  token: 'pat_eVYleDKSIbf1V8tIzTHAygDeuyjPanvpjC6OV5W6vAkW3KAGpBsJIB9uKNw2kMBu',
+  token: '',
   baseURL: COZE_CN_BASE_URL, // Use COZE_CN_BASE_URL for China region
   allowPersonalAccessTokenInBrowser: true
 })
-export const bot_id = '7433399372433440820'
+export const bot_id = ''
